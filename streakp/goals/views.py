e4ec47f804875f732d5e23c531d072e35682e327
Kelemen Kata <kelemenkata@gmail.com>
2015-06-26 15:36:08 +0100

    one date per day

--- streakp/goals/views.py
+++ streakp/goals/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils import timezone
+from datetime import datetime
 from .models import Goal, Day
 
+def same_date(date1):
+    return date1 == datetime.today().date()
+
+def allow_create(goal):
+    days = goal.day_set.all()
+    if not days: return True
+    else:
+        last_entry = days.latest('id').date
+        last_entry_date = last_entry.date()
+        return last_entry_date != timezone.now().date()
 
 def index(request):
 
@@ -11,14 +22,15 @@ def index(request):
     context = {'goals': v1}
     return render(request, 'goals/index.html', context)
 
+
 def goal(request, goal_id):
     current_goal = Goal.objects.get(id=goal_id)
     days = current_goal.day_set.all()
     context = {'days': days, 'current_goal':current_goal}
-
-    if request.method=='POST':
+    if request.method=='POST' and allow_create(current_goal):
         d = Day(goal=current_goal, date=timezone.now())
         d.save()
-        return HttpResponse("OK, created!")
+        return render(request, 'goals/goal.html', context)
+            #HttpResponse("OK, created!")
     else:
         return render(request, 'goals/goal.html', context)
