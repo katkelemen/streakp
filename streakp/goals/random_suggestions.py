8dd3cfeeb01450e1c090a2ca4f952b801ad1ba98
Kelemen Kata <kelemenkata@gmail.com>
2015-08-11 20:02:33 +0100

    requirements.txt updated

--- streakp/goals/random_suggestions.py
+++ streakp/goals/random_suggestions.py
@@ -1,6 +1,10 @@
 import random
 
-rand_sug = ["practice moondlanding", "pet a panda"]
+rand_sug = ["practice moondlanding",
+            "hug a panda",
+            "learn a kanji",
+            "make a bacon pancake",
+            "feed my dragon"]
 
 def random_suggestions():
     return random.choice(rand_sug)
